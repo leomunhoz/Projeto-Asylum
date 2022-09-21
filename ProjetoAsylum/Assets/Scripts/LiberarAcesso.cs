6cd7b5cdc674b347655b4b9560a81f2ca69ae5ec
leomunhoz <lmpaixao70@gmail.com>
2022-09-21 01:44:05 -0300

    upd 10
    - abrir porta ataves do painel de energia

--- ProjetoAsylum/Assets/Scripts/LiberarAcesso.cs
+++ ProjetoAsylum/Assets/Scripts/LiberarAcesso.cs
@@ -5,12 +5,53 @@ using UnityEngine;
 public class LiberarAcesso : MonoBehaviour
 {
     
+    private static int count = 0;
+    public Animator porta;
+    public Animator ligar;
+    bool EstaTrigado;
+
     void Start()
     {
         
     }
+
+     void Update()
+    {
+        if (EstaTrigado)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && gameObject.tag == "Ligar")
+            {
+                count++;
+                ligar.SetTrigger("Ligar");
+                gameObject.tag = "Ligado";
+            }
+           
+        }
+
+        if (count == 4)
+        {
+            porta.SetTrigger("Abrir");
+            Debug.Log("Aberto");
+            
+        }
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
-       
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EstaTrigado = true;
+        }
+
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EstaTrigado = false;
+        }
+    }
+
+
+
 }
