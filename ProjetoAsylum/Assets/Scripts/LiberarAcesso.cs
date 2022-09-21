3550314be9f8834c16b0b1e0fb90976fc8353950
leomunhoz <101760339+leomunhoz@users.noreply.github.com>
2022-09-20 22:17:06 -0300

    upd 09

---
+++ ProjetoAsylum/Assets/Scripts/LiberarAcesso.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiberarAcesso : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+}
