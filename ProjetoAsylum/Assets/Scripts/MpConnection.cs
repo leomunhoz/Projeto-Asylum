0d95bd70c6b5787ca228deaf794de5cff6258ec1
leomunhoz <101760339+leomunhoz@users.noreply.github.com>
2022-10-10 23:58:44 -0300

    upd 17.01

---
+++ ProjetoAsylum/Assets/Scripts/MpConnection.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MultiplayerConnection : MonoBehaviourPunCallbacks
+{
+    public Text connectionLog;
+    public Player mySelf;
+
+    //--------------------------------------------------------
+    void Start()
+    {
+        PhotonNetwork.LocalPlayer.NickName = System.Environment.UserName;
+        connectionLog.text = "Conectando... como: " + PhotonNetwork.LocalPlayer.NickName + "\n";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //--------------------------------------------------------
+    public override void OnConnectedToMaster()
+    {
+        connectionLog.text += "Conectado ao servidor!\n";
+        connectionLog.text += "Entrando no lobby...\n";
+        PhotonNetwork.JoinLobby();
+    }
+
+    //--------------------------------------------------------
+    public override void OnJoinedLobby()
+    {
+        connectionLog.text += "Entrou no lobby!\n";
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 5;
+
+        connectionLog.text += "Entrando na SALA: PUCC\n";
+        PhotonNetwork.JoinOrCreateRoom("PUCC", roomOptions, null);
+    }
+
+    //--------------------------------------------------------
+    public override void OnJoinedRoom()
+    {
+        connectionLog.text += "Entrei na SALA: PUCC!\n";
+
+        Vector3 pos = Random.insideUnitSphere * 5;
+        pos.y = 1;
+
+        GameObject player = PhotonNetwork.Instantiate("Player", pos, Quaternion.identity);
+        mySelf = player.GetComponent<Player>();
+    }
+
+    //--------------------------------------------------------
+  
+    
+
+    //--------------------------------------------------------
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        connectionLog.text += "Player: " + newPlayer.NickName + " entrou na SALA: PUCC!\n";
+       
+    }
+
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    {
+        connectionLog.text += "Player: " + otherPlayer.NickName + " saiu da SALA: PUCC!\n";
+    }
+}
